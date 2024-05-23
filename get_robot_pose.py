2873c19731ff53fa78f071f744a8c69da0f94799
arthur801031 <arthur801031@gmail.com>
2024-05-23 12:36:43 -0700

    Update get_robot_pose

--- get_robot_pose.py
+++ get_robot_pose.py
@@ -2,8 +2,12 @@ import urx_local
 import time
 
 if __name__ == '__main__':
-    robot = urx_local.Robot("192.10.0.11")
-    trans = robot.get_pose()
-    print('Robot pose: ', trans)
+    robot_left = urx_local.Robot("192.10.0.11")
+    trans = robot_left.get_pose()
+    print('Left robot pose: ', trans)
     time.sleep(1)
+
+    robot_right = urx_local.Robot("192.10.0.12")
+    trans = robot_right.get_pose()
+    print('Right robot pose: ', trans)
     print('Done')
